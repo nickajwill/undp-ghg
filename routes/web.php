d14201313e76f80c641cc06d83bf393d0846b9ed
Nick Williams <nickajwill@gmail.com>
2017-06-15 19:43:19 +0000

    Edits to auth screens and routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
